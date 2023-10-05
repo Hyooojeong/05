c45c205ff57b390b74c268d5723593ae176e16c8
Hyooojeong <hjj001028@naver.com>
2023-10-05 16:17:37 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int num;
+	char c;
+	int num = 0;
 	
-	printf("정수 하나를 입력하시오 : ");
-	scanf("%d", &num);
+	printf("input a string: ");
+	scanf("%c", &c);
 	
-	if (num >= 0)
-	printf("절대값은 %d입니다.", num);
+	while((c = getchar())!='\n') {
+		
+		if(c >= '0' && c <= '9')
+		num = num + 1;
+	}	
 	
-	else
-	printf("절대값은 %d입니다.", abs(num));
+	printf("the number of digits is %i\n", num);
 	
+	system("PAUSE");
 	return 0;
 }
